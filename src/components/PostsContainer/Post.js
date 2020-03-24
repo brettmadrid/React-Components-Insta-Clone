f1f0483ec97496b037d18021cabe1ebea8bfc3f3
brettmadrid <brettmadrid9@gmail.com>
2020-03-23 22:30:26 -0700

    added id to dummy data

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,5 +1,5 @@
 // You will add code in this file
-import React from "react";
+import React, { useState } from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
@@ -7,33 +7,26 @@ import PostHeader from "./PostHeader";
 import "./Posts.css";
 
 // pass props in this file to
-const Post = props => {
-  // set up state for the likes
+const Post = ({
+  username,
+  thumbnailUrl,
+  imageUrl,
+  likes,
+  timestamp,
+  comments
+}) => {
+  const [like, setLikes] = useState(likes);
 
   return (
     <div className="post-border">
-      <PostHeader
-        username={props.post.username}
-        thumbnailUrl={
-          props.post.thumbnailUrl
-        }
-      />
+      <PostHeader username={username} thumbnailUrl={thumbnailUrl} />
       <div className="post-image-wrapper">
-        <img
-          alt="post thumbnail"
-          className="post-image"
-          src={props.post.imageUrl}
-        />
+        <img alt="post thumbnail" className="post-image" src={imageUrl} />
       </div>
-      <LikeSection />
-      <CommentSection
-        postId={props.post.imageUrl}
-        comments={props.post.comments}
-      />
+      <LikeSection likes={like} addLikes={setLikes} />
+      <CommentSection postId={imageUrl} comments={comments} />
     </div>
   );
 };
 
 export default Post;
-
-
